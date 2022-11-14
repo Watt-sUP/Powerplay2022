0254ead6d47de4929e33696674d3935d2cf49490
Dicu69 <dicustefan99@yahoo.com>
2022-11-14 18:15:23 +0200

    dicu glisiere doua motoare test momentan

--- FtcRobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Glisiere.java
+++ FtcRobotController/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Glisiere.java
@@ -20,11 +20,11 @@ public class Glisiere {
         motor.setTargetPosition(0);
         motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-        //motor2 = hardwareMap.dcMotor.get(Config.glisiera);
-        //motor2.setDirection(DcMotorSimple.Direction.REVERSE);
-        //motor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        //motor2.setTargetPosition(0);
-        //motor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motor2 = hardwareMap.dcMotor.get(Config.glisiera);
+        motor2.setDirection(DcMotorSimple.Direction.REVERSE);
+        motor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor2.setTargetPosition(0);
+        motor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
 
     public void setToPosition(int position) {
@@ -33,10 +33,10 @@ public class Glisiere {
         if (position != 0) motor.setPower(1);
         else motor.setPower(1);
 
-        //motor2.setTargetPosition(positions[position]);
-        /// motor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        //if(position != 0) motor2.setPower(0.7);
-        // else motor2.setPower(0.7);
+        motor2.setTargetPosition(positions[position]);
+        motor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        if(position != 0) motor2.setPower(1);
+         else motor2.setPower(1);
     }
 
     public void setToTicks(int ticks) {
@@ -44,9 +44,9 @@ public class Glisiere {
         motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         motor.setPower(1);
 
-        //motor2.setTargetPosition(ticks);
-        //motor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        //motor2.setPower(0.2);
+        motor2.setTargetPosition(ticks);
+        motor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motor2.setPower(1);
     }
 
     public void modifyPosition(int ticks) {
@@ -54,9 +54,9 @@ public class Glisiere {
         motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         motor.setPower(0.2);
 
-        //motor2.setTargetPosition(motor.getCurrentPosition() + ticks);
-        //motor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        //motor2.setPower(0.2);
+        motor2.setTargetPosition(motor.getCurrentPosition() + ticks);
+        motor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motor2.setPower(0.2);
     }
 
     public double getPosition() {
